088650125461fce3395a0357745d11cd83ef4c6c
lgalant <lgalant@live.com>
2026-08-09 15:26:30 -0300

    res

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const {Client} = pkg;
 const client = new Client(dbconfig)
 await client.connect()
 
-const result = await client.query("SELECT * FROM usuario")
+const result = await client.query("SELECT * FROM usuario order by id")
 console.log(result.rows)
+const usuario1 = result.rows[0].nombre
+console.log("usuario1:",usuario1)
 
 await client.end()
 
 const app = express()
 const port = 3000;
-app.get('/',(req,res)=>res.send("Welcome avanzados 2026!"))
+app.get('/',(req,res)=>res.send("Welcome " + usuario1 ))
 app.listen(port,() => {console.log(`listening on http://localhost:${port}`)})
