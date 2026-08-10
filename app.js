ead2a9bb4464d2d4e8c2d3ce562d3a7103fde7c0
lgalant <lgalant@live.com>
2026-08-09 23:08:35 -0300

    mejoras vercel

---
+++ app.js
@@ -0,0 +1,20 @@
+import pkg from 'pg'
+import dbconfig from './dbconfig.js'
+import express from 'express'
+
+const {Client} = pkg;
+const client = new Client(dbconfig)
+await client.connect()
+
+const result = await client.query("SELECT * FROM usuario order by id")
+console.log(result.rows)
+const usuario1 = result.rows[0].nombre
+console.log("usuario1:",usuario1)
+
+await client.end()
+
+const app = express()
+const port = 3000;
+app.get('/',(req,res)=>res.send("Welcome " + usuario1 ))
+//app.listen(port,() => {console.log(`listening on http://localhost:${port}`)})
+export default app;
